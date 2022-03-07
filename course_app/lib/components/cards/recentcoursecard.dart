78deaf1d8904c5fb29d5734ae8053d4b2142748f
Sumith Damodaran <sumithpd@gmail.com>
2022-03-08 00:35:02 +0100

    Course card with details

--- course_app/lib/components/cards/recentcoursecard.dart
+++ course_app/lib/components/cards/recentcoursecard.dart
@@ -10,59 +10,81 @@ class RecentCourseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 20.0),
-      child: Container(
-        width: 240,
-        height: 240,
-        decoration: BoxDecoration(
-          gradient: course.background,
-          borderRadius: BorderRadius.circular(41.0),
-          boxShadow: [
-            BoxShadow(
-                color: course.background.colors[0].withOpacity(0.3),
-                offset: const Offset(0, 20),
-                blurRadius: 30.0),
-            BoxShadow(
-                color: course.background.colors[1].withOpacity(0.3),
-                offset: const Offset(0, 20),
-                blurRadius: 30.0)
-          ],
-        ),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(
-                top: 32.0,
-                left: 32.0,
-                right: 32.0,
+    return Stack(alignment: Alignment.topRight, children: [
+      Padding(
+        padding: const EdgeInsets.only(top: 20.0),
+        child: Container(
+          width: 240,
+          height: 240,
+          decoration: BoxDecoration(
+            gradient: course.background,
+            borderRadius: BorderRadius.circular(41.0),
+            boxShadow: [
+              BoxShadow(
+                  color: course.background.colors[0].withOpacity(0.3),
+                  offset: const Offset(0, 20),
+                  blurRadius: 30.0),
+              BoxShadow(
+                  color: course.background.colors[1].withOpacity(0.3),
+                  offset: const Offset(0, 20),
+                  blurRadius: 30.0)
+            ],
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 32.0,
+                  left: 32.0,
+                  right: 32.0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      course.courseSubtitle,
+                      style: kCardSubtitleStyle,
+                    ),
+                    SizedBox(
+                      height: 6.0,
+                    ),
+                    Text(
+                      course.courseTitle,
+                      style: kCardTitleStyle,
+                    ),
+                  ],
+                ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    course.courseSubtitle,
-                    style: kCardSubtitleStyle,
-                  ),
-                  SizedBox(
-                    height: 6.0,
-                  ),
-                  Text(
-                    course.courseTitle,
-                    style: kCardTitleStyle,
-                  ),
-                ],
+              Expanded(
+                child: Image.asset(
+                  'asset/illustrations/${course.illustration}',
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Expanded(
-              child: Image.asset(
-                'asset/illustrations/${course.illustration}',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-    );
+      Padding(
+        padding: EdgeInsets.only(right: 42.0),
+        child: Container(
+          child: Image.asset('asset/logos/${course.logo}'),
+          width: 60.0,
+          height: 60.0,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(18.0),
+            boxShadow: [
+              BoxShadow(
+                color: kShadowColor,
+                offset: Offset(0, 4),
+                blurRadius: 16.0,
+              ),
+            ],
+          ),
+          padding: EdgeInsets.all(12.0),
+        ),
+      ),
+    ]);
   }
 }
