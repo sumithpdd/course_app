50b3ba0bc68e1e18c34b5cd964c996a49f31303e
Sumith Damodaran <sumithpd@gmail.com>
2022-03-07 18:45:21 +0100

    box with icon

--- course_app/lib/main.dart
+++ course_app/lib/main.dart
@@ -10,10 +10,41 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Container(
-            child: const Text("Hello World"),
-          ),
-        ),
+            child: Row(
+          children: [
+            Container(
+              width: 42.0,
+              height: 42.0,
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(14.0),
+                gradient: const LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xFF00AEFF),
+                    Color(0xFF0076FF),
+                  ],
+                ),
+              ),
+              child: const Icon(
+                Icons.home,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(width: 12),
+            Container(
+              child: const Text(
+                "Home",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w800,
+                  color: const Color(0xFF242629),
+                ),
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
