02abb1237c74a605dc36a6c04b40cae092904e39
Sumith Damodaran <sumithpd@gmail.com>
2022-03-07 22:12:09 +0100

    SidebarScreen

--- course_app/lib/main.dart
+++ course_app/lib/main.dart
@@ -1,52 +1,20 @@
 import 'package:flutter/material.dart';
 
-import 'model/sidebar.dart';
+import 'screens/sidebar_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      body: Center(child: SidebarRow(item: sidebarItem[3])),
-    ));
-  }
-}
-
-class SidebarRow extends StatelessWidget {
-  SidebarRow({required this.item});
-
-  final SidebarItem item;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          width: 42.0,
-          height: 42.0,
-          padding: EdgeInsets.all(10.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(14.0),
-            gradient: item.background,
-          ),
-          child: item.icon,
-        ),
-        SizedBox(width: 12),
-        Container(
-          child: Text(
-            item.title,
-            style: TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.w800,
-              color: Color(0xFF242629),
-            ),
-          ),
-        ),
-      ],
-    );
+          body: SidebarScreen(),
+        ));
   }
 }
