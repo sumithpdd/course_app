f9ba0cf0cf9d525ccb85bc86fcb82f1566d95570
Sumith Damodaran <sumithpd@gmail.com>
2022-03-08 11:05:47 +0100

    refactor code

---
+++ course_app/lib/components/lists/explorecourselist.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../model/course.dart';
+import '../cards/explorecoursecard.dart';
+
+class ExploreCourseList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemCount: exploreCourses.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.only(left: index == 0 ? 20.0 : 0.0),
+            child: ExploreCourseCard(course: exploreCourses[index]),
+          );
+        },
+      ),
+    );
+  }
+}
--- course_app/lib/main.dart
+++ course_app/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:course_app/constants.dart';
 import 'package:flutter/material.dart';
 
 import 'components/homescreennavbar.dart';
+import 'components/lists/explorecourselist.dart';
 import 'components/lists/recentcourselist.dart';
 import 'model/course.dart';
 
@@ -67,77 +68,5 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ));
-  }
-}
-
-class ExploreCourseList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 120,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        itemCount: exploreCourses.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.only(left: index == 0 ? 20.0 : 0.0),
-            child: ExploreCourseCard(course: exploreCourses[index]),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class ExploreCourseCard extends StatelessWidget {
-  ExploreCourseCard({required this.course});
-
-  final Course course;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(right: 20.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(41.0),
-        child: Container(
-          height: 120.0,
-          width: 280.0,
-          decoration: BoxDecoration(gradient: course.background),
-          child: Padding(
-            padding: EdgeInsets.only(left: 32.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        course.courseSubtitle,
-                        style: kCardSubtitleStyle,
-                      ),
-                      SizedBox(height: 6.0),
-                      Text(
-                        course.courseTitle,
-                        style: kCardTitleStyle,
-                      ),
-                    ],
-                  ),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Image.asset('asset/illustrations/${course.illustration}',
-                        fit: BoxFit.cover, height: 100.0)
-                  ],
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
